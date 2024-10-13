c9350f505eb64e5073901d55022900b2ea7f6e06
TAEBAN\kwth1 <kwth2002hs@naver.com>
2024-10-13 20:36:13 +0900

    회원가입 정규화 아이디 비밀번호 찾기 만들기

--- demoim/src/main/resources/static/js/signup/signup.js
+++ demoim/src/main/resources/static/js/signup/signup.js
@@ -13,6 +13,7 @@ function changeEmail() {
     emailKindInput.value = selectedValue; // 선택된 값 입력 필드에 설정
     emailKindInput.disabled = true; // 입력 필드 비활성화
   }
+<<<<<<< HEAD
 }
 
 // 이메일 유효성 검사
@@ -99,3 +100,105 @@ new daum.Postcode({
 // 주소 검색 버튼 클릭 시
 const searchAddress = document.querySelector("#searchAddress");
 searchAddress.addEventListener("click", findAddress);
+=======
+// 이메일 유효성 검사 해야함
+
+//주소 api
+  function findAddress() {
+    new daum.Postcode({
+      oncomplete: function (data) {
+        // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+  
+        // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+        // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+        var addr = ''; // 주소 변수
+        
+        //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+        if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+          addr = data.roadAddress;
+        } else { // 사용자가 지번 주소를 선택했을 경우(J)
+          addr = data.jibunAddress;
+        }
+  
+        // 우편번호와 주소 정보를 해당 필드에 넣는다.
+        document.getElementById('postcode').value = data.zonecode;
+        document.getElementById("address").value = addr;
+        // 커서를 상세주소 필드로 이동한다.
+        document.getElementById("detailAddress").focus();
+      }
+    }).open();
+  }
+  // 주소 검색 버튼 클릭 시
+const searchAddress = document.querySelector("#searchAddress");
+searchAddress.addEventListener("click", findAddress);
+
+/* 비밀번호 유효성 검사 */
+
+const memberPw = document.querySelector("#memberPw");
+const memberPwConfirm = document.querySelector("#memberPwConfirm");
+const pwMessage = document.querySelector("#pwMessage");
+const pwMessageObj = {};
+pwMessageObj.normal = "영어,숫자,특수문자 1글자 이상, 6~20자 사이.";
+pwMessageObj.invaild = "유효하지 않은 비밀번호 형식입니다.";
+pwMessageObj.vaild = "유효한 비밀번호 형식입니다.";
+pwMessageObj.error = "비밀번호가 일치하지 않습니다.";
+pwMessageObj.check = "비밀번호가 일치 합니다.";
+memberPw.addEventListener("input", () => {
+  const inputPw = memberPw.value.trim();
+  if(inputPw.length === 0){ // 비밀번호 미입력
+    pwMessage.innerText = pwMessageObj.normal;
+    pwMessage.classList.remove("confirm", "error");
+    checkObj.memberPw = false;
+    memberPw.value = "";
+    return;
+  }
+  // 비밀번호 정규표현식 검사
+  const lengthCheck = inputPw.length >= 6 && inputPw.length <= 20;
+  const letterCheck = /[a-zA-Z]/.test(inputPw); // 영어 알파벳 포함
+  const numberCheck = /\d/.test(inputPw); // 숫자 포함
+  const specialCharCheck = /[\!\@\#\_\-]/.test(inputPw); // 특수문자 포함
+  // 조건이 하나라도 만족하지 못하면
+  if ( !(lengthCheck && letterCheck && numberCheck && specialCharCheck) ) {
+    pwMessage.innerText = pwMessageObj.invaild;
+    pwMessage.classList.remove("confirm");
+    pwMessage.classList.add("error");
+    checkObj.memberPw = false;
+    return;
+  }
+  pwMessage.innerText = pwMessageObj.vaild;
+  pwMessage.classList.remove("error");
+  pwMessage.classList.add("confirm");
+  checkObj.memberPw = true;
+  // 비밀번호 확인이 작성된 상태에서
+  // 비밀번호가 입력된 경우
+  if(memberPwConfirm.value.trim().length > 0){
+    checkPw(); // 같은지 비교하는 함수 호출
+  }
+});
+/* ----- 비밀번호, 비밀번호 확인 같은지 검사하는 함수 ----- */
+function checkPw(){
+  // 같은 경우
+  if(memberPw.value === memberPwConfirm.value){
+    pwMessage.innerText = pwMessageObj.check;
+    pwMessage.classList.add("confirm");
+    pwMessage.classList.remove("error");
+    checkObj.memberPwConfirm = true;
+    return;
+  }
+  // 다른 경우
+  pwMessage.innerText = pwMessageObj.error;
+  pwMessage.classList.add("error");
+  pwMessage.classList.remove("confirm");
+  checkObj.memberPwConfirm = false;
+}
+/* ----- 비밀번호 확인이 입력 되었을 때  ----- */
+memberPwConfirm.addEventListener("input", () => {
+  // 비밀번호 input에 작성된 값이 유효한 형식일때만 비교
+  if(checkObj.memberPw === true){
+    checkPw();
+    return;
+  }
+  // 비밀번호 input에 작성된 값이 유효하지 않은 경우
+  checkObj.memberPwConfirm = false;
+});
+>>>>>>> parent of ce55aac (프로젝트 정규 표현식 작성 및 css 수정)
